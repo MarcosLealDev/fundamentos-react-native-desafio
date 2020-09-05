73a27cd8ff02ad18c21d46121f396085bcdac628
Marcos Leal <marcosleal@gmail.com>
2020-09-05 13:50:13 -0400

    Finilized with space for improvement :-)

--- src/pages/Cart/index.tsx
+++ src/pages/Cart/index.tsx
@@ -3,6 +3,8 @@ import FeatherIcon from 'react-native-vector-icons/Feather';
 
 import { View } from 'react-native';
 
+// import FloatingCart from '../../components/FloatingCart';
+
 import {
   Container,
   ProductContainer,
@@ -39,23 +41,33 @@ const Cart: React.FC = () => {
   const { increment, decrement, products } = useCart();
 
   function handleIncrement(id: string): void {
-    // TODO
+    increment(id);
   }
 
   function handleDecrement(id: string): void {
-    // TODO
+    decrement(id);
   }
 
   const cartTotal = useMemo(() => {
     // TODO RETURN THE SUM OF THE QUANTITY OF THE PRODUCTS IN THE CART
+    const total = products.reduce((accumulator, product) => {
+      const subTotal = product.price * product.quantity;
 
-    return formatValue(0);
+      return accumulator + subTotal;
+    }, 0);
+
+    return formatValue(total);
   }, [products]);
 
   const totalItensInCart = useMemo(() => {
     // TODO RETURN THE SUM OF THE QUANTITY OF THE PRODUCTS IN THE CART
+    const total = products.reduce((accumulator, product) => {
+      const subTotal = product.quantity;
 
-    return 0;
+      return accumulator + subTotal;
+    }, 0);
+
+    return total;
   }, [products]);
 
   return (
@@ -105,6 +117,7 @@ const Cart: React.FC = () => {
           )}
         />
       </ProductContainer>
+      {/* <FloatingCart /> */}
       <TotalProductsContainer>
         <FeatherIcon name="shopping-cart" color="#fff" size={24} />
         <TotalProductsText>{`${totalItensInCart} itens`}</TotalProductsText>
